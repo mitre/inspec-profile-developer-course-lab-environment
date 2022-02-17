7abddfb18aee3026085198d7bd9068acc723f385
Aaron Lippold <lippold@gmail.com>
2022-02-17 01:50:36 +0000

    Pending changes exported from your codespace

--- lab-setup.sh
+++ lab-setup.sh
@@ -2,7 +2,6 @@
 if ! command -v inspec &> /dev/null
 then
   echo "Installing InSpec into the Environment"
-  curl -L https://omnitruck.cinc.sh/install.sh | sudo bash -s -- -P cinc-auditor
 fi
 
 docker-compose -f docker-compose.yml up -d
