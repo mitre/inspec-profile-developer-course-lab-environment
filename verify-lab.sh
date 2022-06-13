653c67bb316c1446d42f1401be1318fc77ce6de0
Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>
2022-06-13 08:05:16 -0500

    Update verify-lab script to auto-update mitre/saf

--- verify-lab.sh
+++ verify-lab.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+npm update -g @mitre/saf
 echo "MITRE SAF version: " && saf --version 
 echo "InSpec version: " && inspec --version
 echo "Docker is running the container(s): " &&  docker ps
